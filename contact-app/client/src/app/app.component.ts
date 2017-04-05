0cc1873fb3a7acdc4d53defd2ad093f496b0a475
simakko <sini.makkonen@gmail.com>
2017-04-05 12:36:34 +0300

    Structure modified. Dialog-service created

--- contact-app/client/src/app/app.component.ts
+++ contact-app/client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Contact} from "./contact/contact";
+import {ContactService} from "./contact/services/contact.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Contact list';
+  contacts: Contact[];
+  selectedContact: Contact;
+
+// asks contacts and transfers them
+  constructor(contactService: ContactService){
+    this.contacts = contactService.showContacts();
+  }
+/*
+  contactSelected(contact: Contact){
+    this.selectedContact = contact;
+  }*/
 }
--- contact-app/client/src/app/app.module.ts
+++ contact-app/client/src/app/app.module.ts
@@ -4,17 +4,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
+import {ContactService} from "./contact/services/contact.service";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MaterialModule} from "@angular/material";
+import { ContactDialogComponent } from './contact/contact-list/contact-list-item/contact-dialog/contact-dialog.component';
+import { MapDialogComponent } from './contact/contact-list/contact-list-item/map-dialog/map-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent,
+    ContactDialogComponent,
+    MapDialogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FlexLayoutModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
